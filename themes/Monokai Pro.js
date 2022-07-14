cc0591945b4ea5ba4bf6686d8fa778bac3d13d95
Alfonso Pinto <alfonso.pinto@gmail.com>
2022-07-14 20:58:37 +0200

    feat: add monokai pro theme

---
+++ themes/Monokai Pro.js
@@ -0,0 +1,26 @@
+black       = '#403E41';
+red         = '#FF6188'; // red
+green       = '#A9DC76'; // green
+yellow      = '#FFD866'; // yellow
+blue        = '#FC9867'; // blue
+magenta     = '#AB9DF2'; // pink
+cyan        = '#78DCE8'; // cyan
+white       = '#FCFCFA'; // light gray
+lightBlack  = '#727072'; // medium gray
+lightRed    = '#FF6188'; // red
+lightGreen  = '#A9DC76'; // green
+lightYellow = '#FFD866'; // yellow
+lightBlue   = '#FC9867'; // blue
+lightMagenta= '#AB9DF2'; // pink
+lightCyan   = '#78DCE8'; // cyan
+lightWhite  = '#FCFCFA'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(246,247,236,0.5)');
+t.prefs_.set('foreground-color', '#FCFCFA');
+t.prefs_.set('background-color', '2D2A2E');
